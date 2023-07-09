303e00f4a9fd1c5be56baf684e05f5d44c301c54
Sunny Kumar <sunnykumar23232@gmail.com>
2023-07-09 10:29:19 +0530

    feat: add complete web pages

---
+++ pages/login/LoginValidation.js
@@ -0,0 +1,4 @@
+const form= document.querySelector('form');
+const emailError= document.querySelector('.login_username__input');
+const passwordError= document.querySelector('.login_password__input');
+
